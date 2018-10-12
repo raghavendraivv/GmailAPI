82c80defc76febdcc9a8de1f9e4bd5851c9f32d3
kvsnagasai <33051967+kvsnagasai@users.noreply.github.com>
2018-10-13 00:03:18 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -8,7 +8,6 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
